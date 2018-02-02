ebe6e5b82698af663ac7f31057e46dabc470701a
double <1283268128@qq.com>
2018-02-02 18:27:02 +0800

    change computed for hide/show list

--- js/shopping_cart.js
+++ js/shopping_cart.js
@@ -16,6 +16,7 @@ var goodsTable = new Vue({
     nm: "",
     //是否结算
     suc: false,
+    fButton: "",//显示分类列表属性
     //isAdd: true,
   	// 默认的商品列表
     goods: [
@@ -54,6 +55,29 @@ var goodsTable = new Vue({
           costPaid += parseInt(this.goods[i].price * this.goods[i].add_nums);
       }
       return costPaid;
+    },
+    shoppingListFilter: function() { //通过计算属性返回列表，实现分类
+      var goods = this.goods;
+      var temp = [];
+      if(this.fButton == '' || this.fButton == '全部'){
+
+        return goods;
+      }else if(this.fButton == '已加入'){
+        for(var i in goods){
+          if(goods[i].add_nums != 0){
+           temp.push(JSON.parse(JSON.stringify(goods[i])));
+          }
+        }
+        return temp;
+      }else if(this.fButton == '未加入'){
+        for(var i in goods){
+          if(goods[i].add_nums == 0){
+            temp.push(JSON.parse(JSON.stringify(goods[i])));
+          }
+        }
+        return temp;
+      }
+      
     }
   },
   methods: {
@@ -81,7 +105,8 @@ var goodsTable = new Vue({
       }else{
         this.goods.splice(index,1);
         //删除buyLists中的商品
-        for(var i=this.buyLists.length-1; i>=0; i--){
+        var len = this.buyLists.length;
+        for(var i=len-1; i>=0; i--){
           if(this.buyLists[i].name == nm){
               this.buyLists.splice(i,1);
           }
@@ -91,22 +116,21 @@ var goodsTable = new Vue({
   	addToCar: function(good) {
   		if(good.add_nums == good.default_nums) return;
   		good.add_nums = good.default_nums;
-      //good.ischange = 'btn-success';
+
       this.goodItem = {name: good.name, nums: good.add_nums};
       // 创建用户当前添加的商品对象
   		// 开始向数组中提添加当前物品，这里存在3种情况
       // 1、用户未添加过该商品，则直接向数组中push
       // 2、用于已经添加了该商品、并且未做购买数量修改，则不向数组中添加
       // 3、用于已经添加了该商品、但是修改了购买数量，直接替换数组中的该商品对象
-      var index = this.buyLists.findIndex(function(value, index, arr) {
-         return value.name === this.goodItem.name;
+      var index = this.buyLists.findIndex((value, index, arr) => { //ES6对数组扩展查找方法
+         return value.name == this.goodItem.name;
       });
       //Object.assign() 方法用于将所有可枚举属性的值从一个或多个源对象复制到目标对象。它将返回目标对象。
       index === -1 ? this.buyLists.push(this.goodItem) : Object.assign(this.buyLists[index], this.goodItem);
     },
     balance: function() {
       if(this.suc){
-
           console.log(this.buyLists);
           if(this.buyLists.length != 0){
             this.msg = '结算成功！';
@@ -116,8 +140,8 @@ var goodsTable = new Vue({
             console.log("购物车还没有加入商品,请先加入到购物车！");
           }
       }
-    },
-    showFunction: function(n) {//$ref 操作DOM
+    }/*,
+    showFunction: function(n) {//$ref 操作DOM 显示或隐藏列表
       if(n == '0'){
         for(var i in this.goods){
           if(this.goods[i].add_nums != 0){
@@ -139,11 +163,11 @@ var goodsTable = new Vue({
           }
         }
       }
-   }
+   }*/
   },
-  updated: function(){//数据更新完成后
+  updated: function() {//数据更新完成后
       //this.$refs.good[0].style.display == "none";
-      console.log(this.$refs.good);
-      console.log(this.$el);
+      //console.log(this.$refs.good);
+      //console.log(this.$el);
   }
 })
